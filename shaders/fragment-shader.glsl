782f58fb77bd790f6003d2e61e0d78e952cd2998
Kyle Adkins <kyle.m.adkins@gmail.com>
2025-05-14 00:24:20 -0400

    Lerp uniform colors

--- shaders/fragment-shader.glsl
+++ shaders/fragment-shader.glsl
@@ -1,5 +1,9 @@
 varying vec2 vUvs;
 
+uniform vec4 color1;
+uniform vec4 color2;
+
 void main() {
-  gl_FragColor = vec4(vUvs.x, vUvs.y, vUvs.x, 1.0);
+  /* mix is just lerp */
+  gl_FragColor = mix(color1, color2, vUvs.x);
 }
