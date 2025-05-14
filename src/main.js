e6456827569d6df43f4b5fa593bb103b00963e70
Kyle Adkins <kyle.m.adkins@gmail.com>
2025-05-13 23:33:09 -0400

    Init commit

---
+++ src/main.js
@@ -0,0 +1,65 @@
+import * as THREE from "three";
+
+import "./style.css";
+
+class App {
+  constructor() {}
+
+  async init() {
+    this._renderer = new THREE.WebGLRenderer();
+    document.body.appendChild(this._renderer.domElement);
+
+    window.addEventListener(
+      "resize",
+      () => {
+        this._onWindowResize();
+      },
+      false
+    );
+
+    this._scene = new THREE.Scene();
+
+    this._camera = new THREE.OrthographicCamera(0, 1, 1, 0, 0.1, 1000);
+    this._camera.position.set(0, 0, 1);
+
+    await this._setupProject();
+
+    this._onWindowResize();
+    this._raf();
+  }
+
+  async _setupProject() {
+    const vsh = await fetch("./shaders/vertex-shader.glsl");
+    const fsh = await fetch("./shaders/fragment-shader.glsl");
+
+    const material = new THREE.ShaderMaterial({
+      uniforms: {},
+      vertexShader: await vsh.text(),
+      fragmentShader: await fsh.text(),
+    });
+
+    const geometry = new THREE.PlaneGeometry(1, 1);
+
+    const plane = new THREE.Mesh(geometry, material);
+    plane.position.set(0.5, 0.5, 0);
+    this._scene.add(plane);
+
+    this._onWindowResize();
+  }
+
+  _onWindowResize() {
+    this._renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  _raf() {
+    requestAnimationFrame((t) => {
+      this._renderer.render(this._scene, this._camera);
+      this._raf();
+    });
+  }
+}
+
+window.addEventListener("DOMContentLoaded", async () => {
+  const app = new App();
+  await app.init();
+});
